b6447fb0cba81e4a7ca4da0d76cc8a755e2873ae
James Blacklock <jamesblacklock@protonmail.com>
2022-08-11 13:47:49 -0400

    jabberwocky

--- api/server.js
+++ api/server.js
@@ -8,6 +8,10 @@ server.get('/', (req, res) => {
     res.send('Hello, world!');
 });
 
+server.get('/jabberwocky', (req, res) => {
+    res.send(`<h1>Jabberwocky</h1><p>${process.env.JABBERWOCKY}</p>`);
+});
+
 server.post('/', (req, res) => {
     res.json(req.body);
 });
